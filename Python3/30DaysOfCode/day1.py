a02e4303cba6a00f9066f7c27b5da45d549e25e6
Jonathan Strickland <djanatyn@gmail.com>
2018-09-02 13:08:54 -0400

    add boilerplate for 30 days of code day 1

--- Python3/30DaysOfCode/day1.py
+++ Python3/30DaysOfCode/day1.py
@@ -1,11 +1,14 @@
 #!/bin/python3
 
+i = 4
+d = 4.0
+s = 'HackerRank '
+
 # Declare second integer, double, and String variables.
 # Read and save an integer, double, and String to your variables.
 i2 = int(input())
 d2 = double(input())
 s2 = String(input())
-
 
 # Print the sum of both integer variables on a new line.
 print(i+i2)
